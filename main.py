76efa9a2e1ce8dac43355fc825ec9fd81d590d21
CaptainYunti <n.pawcio@gmail.com>
2025-01-18 22:43:43 +0100

    update learning

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ device = (
 
 print(f"Using device: {device}\n")
 
-epochs = 10
-
 model_128 = my_models.OneHiddenLayer(128).to(device)
 model_256 = my_models.OneHiddenLayer(256).to(device)
 model_512 = my_models.OneHiddenLayer(512).to(device)
@@ -39,13 +37,15 @@ model_512_512_128 = my_models.ThreeHiddenLayer((512,512,128)).to(device)
 #models = [model_512_512_512, model_512_512_128]
 #models = [model_512, model_512_512, model_512_512_128]
 
+
+
+epochs = 10
+
 models = [model_128, model_256, model_512, model_512_256, model_512_512, model_512_512_128, model_512_512_512]
-
-
 loss_fn = nn.CrossEntropyLoss()
 optimizers = [torch.optim.SGD(model.parameters(), lr=1e-3) for model in models]
 
-print("Learning ...")
+print("Learning (10) ...")
 
 for indx, model in enumerate(models):
     print(f"Model: {model.extra_repr()}")
@@ -56,21 +56,24 @@ for indx, model in enumerate(models):
         print("Done!\n\n")
 
 
-torch.save(model_128.state_dict(), "models/model_128_relu_10.pth")
-torch.save(model_256.state_dict(), "models/model_256_relu_10.pth")
-torch.save(model_512.state_dict(), "models/model_512_relu_10.pth")
+torch.save(model_128.state_dict(), "models/model_128_relu_sgd_10.pth")
+torch.save(model_256.state_dict(), "models/model_256_relu_sgd_10.pth")
+torch.save(model_512.state_dict(), "models/model_512_relu_sgd_10.pth")
 
-torch.save(model_512_256.state_dict(), "models/model_512_256_relu_10.pth")
-torch.save(model_512_512.state_dict(), "models/model_512_512_relu_10.pth")
+torch.save(model_512_256.state_dict(), "models/model_512_256_relu_sgd_10.pth")
+torch.save(model_512_512.state_dict(), "models/model_512_512_relu_sgd_10.pth")
 
-torch.save(model_512_512_128.state_dict(), "models/model_512_512_128_relu_10.pth")
-torch.save(model_512_512_512.state_dict(), "models/model_512_512_512_relu_10.pth")
+torch.save(model_512_512_128.state_dict(), "models/model_512_512_128_relu_sgd_10.pth")
+torch.save(model_512_512_512.state_dict(), "models/model_512_512_512_relu_sgd_10.pth")
 
 
 
 epochs = 30
 
-print("Learning ...")
+models = [model_512, model_512_512, model_512_512_128]
+optimizers = [torch.optim.SGD(model.parameters(), lr=1e-3) for model in models]
+
+print("Learning (30) ...")
 
 for indx, model in enumerate(models):
     print(f"Model: {model.extra_repr()}")
@@ -81,6 +84,6 @@ for indx, model in enumerate(models):
         print("Done!\n\n")
 
 
-torch.save(model_512.state_dict(), "models/model_512_relu_30.pth")
-torch.save(model_512_512.state_dict(), "models/model_512_512_relu_30.pth")
-torch.save(model_512_512_128.state_dict(), "models/model_512_512_128_relu_30.pth")
+torch.save(model_512.state_dict(), "models/model_512_relu_sgd_30.pth")
+torch.save(model_512_512.state_dict(), "models/model_512_512_relu_sgd_30.pth")
+torch.save(model_512_512_128.state_dict(), "models/model_512_512_128_relu_sgd_30.pth")
